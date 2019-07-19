7cfeae31328c4b2e301f5266e7fb504b3306d96e
Waleed Meselhy <waleed_meselhy@eng.asu.edu.eg>
2019-07-19 16:21:01 +0200

    add docker-compose file

--- scrap_us_proxy/settings.py
+++ scrap_us_proxy/settings.py
@@ -91,4 +91,4 @@ DOWNLOADER_MIDDLEWARES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-SPLASH_URL = 'http://localhost:8050'
+SPLASH_URL = 'http://splash:8050'
